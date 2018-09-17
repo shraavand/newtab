a1010eb677e9b5a36a5e5ebf2fc43ede29ead01c
shraavand <shraavand@yahoo.com>
2018-09-17 15:18:08 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,6 +3,9 @@ function newQuote() {
     var randomNumber = Math.floor(Math.random() * (quotes.length));
     document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
 }
+var quotes = [ '"I thought you put it on afterwards"', '"Whats wrong with that?"', '"2+2=orange, look under the storage"', '"OHM is a hottie"', '"Whats up guys omega herobrine back with another video"', '"Perhaps two easy"', '"Via an array of sunshine"', '"28 point lead"', '"The cool down period bewtween BTS and QL"', '"Bingo"', '"The wire strippers"', '"Johns TCPDPDTSJHFSKJAD sever"', '"Was that one catch worth it?"', '"My sub caught on fire/smoke"', '"Lets bring Elon Musk to our school"', '"RIP HIRSCH"', '"KAPPA KAPPA"', '"Agent s"', '"Make Healthy Choices"', '"Hey guys __ here back with another video"', '"Extra?!"', '"Im sorry I have to go take a photo"', '"If its red your dead"', '"Go make a blog!"', '"If youre reading this you dont need glasses"', '"Last years reveal video"', '"Your not just editing a video,youre making a story"', '"I dont speak gujurathi"', '"You will thank me later"', '"My pants give me headaches"', '"Oh I know why I have a headache!"',
+
+]
 
 function startTime() {
     var today = new Date();
